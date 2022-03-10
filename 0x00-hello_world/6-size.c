24b51cbb23a26cebbe7105029b963d9b1f69f98a
niyikomeyeeric <niyikomeye@gmail.com>
2022-03-10 20:07:20 +0000

    size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types
+ * Return: 0
+ */
+int main (void)
+{
+	printf("Size of a char: %ld byte(s)\n" , sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	return (0);
+}
